c4781fa68c6ed974c58eb1a72b428d21e2d6911d
Prahalad Belavadi <github@belavadi.com>
2020-02-18 13:33:20 +0530

    render emoji

---
+++ src/components/EmojiDict.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class EmojiDict extends Component {
+    state = {
+        '😃': '😃 Smiley',
+        '🚀': '🚀 Rocket',
+        '⚛️': '⚛️ Atom Symbol'
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>{this.state['😃']}</Text>
+                <Text>{this.state['🚀']}</Text>
+                <Text>{this.state['⚛️']}</Text>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default EmojiDict;
